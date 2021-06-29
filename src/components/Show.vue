9b43d891ec16ef593d8520ba9c94d059bb393f25
TomokoNakatani <67673484+TomokoNakatani@users.noreply.github.com>
2021-06-29 11:26:33 +0900

    詳細画面を作成

---
+++ src/components/Show.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h1>タスク詳細</h1>
+
+    <table>
+      <tr>
+        <th>タイトル</th>
+        <td>{{ title }}</td>
+      </tr>
+      <tr>
+        <th>概要</th>
+        <td>{{ body }}</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  name: "Show",
+  data() {
+    return {
+      title: "",
+      body: "",
+    };
+  },
+  created() {
+    this.title = this.getTasks[this.taskId].title;
+    this.body = this.getTasks[this.taskId].body;
+  },
+
+  computed: {
+    ...mapGetters(["getTasks"]),
+    taskId() {
+      return this.$route.params.id;
+    },
+  },
+};
+</script>
